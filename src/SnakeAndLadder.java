19c145618e68a72802c2d4132eea438684733d64
Dhruv <guptadhruv1908@gmail.com>
2024-12-11 16:02:15 +0530

    uc7 commit

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -2,43 +2,78 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        int playerPos = 0;
+        int player1Pos = 0, player2Pos = 0;
         int winningPos = 100;
         int dieCount = 0;
         Random rand = new Random();
 
-        System.out.println("Player starts at position: " + playerPos);
+        System.out.println("Player 1 starts at position: " + player1Pos);
+        System.out.println("Player 2 starts at position: " + player2Pos);
 
-        while (playerPos < winningPos) {
+        while (player1Pos < winningPos && player2Pos < winningPos) {
             int dieRoll = rand.nextInt(6) + 1;
-            int option = rand.nextInt(3);
+            int option1 = rand.nextInt(3);
+            int option2 = rand.nextInt(3);
 
             dieCount++;
-            System.out.println("Roll #" + dieCount + ": Player rolls the die and gets: " + dieRoll);
+            System.out.println("Roll #" + dieCount);
 
-            switch (option) {
+            // Player 1 turn
+            System.out.println("Player 1 rolls the die and gets: " + dieRoll);
+            switch (option1) {
                 case 0:
-                    System.out.println("No Play: Player stays at position " + playerPos);
+                    System.out.println("No Play: Player 1 stays at position " + player1Pos);
                     break;
                 case 1:
-                    if (playerPos + dieRoll <= winningPos) {
-                        playerPos += dieRoll;
-                        System.out.println("Ladder: Player moves ahead to position " + playerPos);
+                    if (player1Pos + dieRoll <= winningPos) {
+                        player1Pos += dieRoll;
+                        System.out.println("Ladder: Player 1 moves ahead to position " + player1Pos);
                     } else {
-                        System.out.println("Ladder: Player stays at position " + playerPos);
+                        System.out.println("Ladder: Player 1 stays at position " + player1Pos);
                     }
                     break;
                 case 2:
-                    playerPos -= dieRoll;
-                    if (playerPos < 0) {
-                        playerPos = 0;
+                    player1Pos -= dieRoll;
+                    if (player1Pos < 0) {
+                        player1Pos = 0;
                     }
-                    System.out.println("Snake: Player moves behind to position " + playerPos);
+                    System.out.println("Snake: Player 1 moves behind to position " + player1Pos);
+                    break;
+            }
+
+            if (player1Pos == winningPos) {
+                break;
+            }
+
+            System.out.println("Player 2 rolls the die and gets: " + dieRoll);
+            switch (option2) {
+                case 0:
+                    System.out.println("No Play: Player 2 stays at position " + player2Pos);
+                    break;
+                case 1:
+                    if (player2Pos + dieRoll <= winningPos) {
+                        player2Pos += dieRoll;
+                        System.out.println("Ladder: Player 2 moves ahead to position " + player2Pos);
+                    } else {
+                        System.out.println("Ladder: Player 2 stays at position " + player2Pos);
+                    }
+                    break;
+                case 2:
+                    player2Pos -= dieRoll;
+                    if (player2Pos < 0) {
+                        player2Pos = 0;
+                    }
+                    System.out.println("Snake: Player 2 moves behind to position " + player2Pos);
                     break;
             }
         }
 
-        System.out.println("Congratulations! Player reaches the exact winning position: " + playerPos);
-        System.out.println("Total number of die rolls to win the game: " + dieCount);
+        if (player1Pos == winningPos) {
+            System.out.println("Player 1 wins the game!");
+        } else {
+            System.out.println("Player 2 wins the game!");
+        }
+
+        System.out.println("Total number of die rolls: " + dieCount);
     }
 }
