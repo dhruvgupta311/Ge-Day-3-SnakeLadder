e1e1347da9c57d32c138ecaa6771b068843951e3
Dhruv <guptadhruv1908@gmail.com>
2024-12-11 15:39:18 +0530

    uc1 commit

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int playerPos = 0;
+        System.out.println("Welcome to the Snake and Ladder Game!");
+        System.out.println("Player starts at position: " + playerPos);
+    }
+}
