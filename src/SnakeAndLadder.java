701d3a1103bc4ff854d162aec8223408c6a2d6d1
Dhruv <guptadhruv1908@gmail.com>
2024-12-11 15:51:23 +0530

    uc3 commit

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -4,8 +4,27 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         int playerPos = 0;
         Random rand = new Random();
+
         int dieRoll = rand.nextInt(6) + 1;
-        System.out.println("Player starts at position: " + playerPos);
-        System.out.println("Player1 rolls the die and gets: " + dieRoll);
+        int option = rand.nextInt(3);
+
+        System.out.println("Player rolls the die and gets: " + dieRoll);
+
+        switch (option) {
+            case 0:
+                System.out.println("No Play: Player stays at position " + playerPos);
+                break;
+            case 1:
+                playerPos += dieRoll;
+                System.out.println("Ladder: Player moves ahead to position " + playerPos);
+                break;
+            case 2:
+                playerPos += dieRoll;
+                if (playerPos < 0) {
+                    playerPos = 0;
+                }
+                System.out.println("Snake: Player moves behind to position " + playerPos);
+                break;
+        }
     }
 }
