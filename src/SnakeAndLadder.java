0eabe22cc8becc190d151e6755af401eb93942dc
Dhruv <guptadhruv1908@gmail.com>
2024-12-11 15:53:33 +0530

    uc4 commit

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -3,28 +3,38 @@ import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int playerPos = 0;
+        int winningPos = 100;
         Random rand = new Random();
 
-        int dieRoll = rand.nextInt(6) + 1;
-        int option = rand.nextInt(3);
+        System.out.println("Player starts at position: " + playerPos);
 
-        System.out.println("Player rolls the die and gets: " + dieRoll);
+        while (playerPos < winningPos) {
+            int dieRoll = rand.nextInt(6) + 1;
+            int option = rand.nextInt(3);
 
-        switch (option) {
-            case 0:
-                System.out.println("No Play: Player stays at position " + playerPos);
-                break;
-            case 1:
-                playerPos += dieRoll;
-                System.out.println("Ladder: Player moves ahead to position " + playerPos);
-                break;
-            case 2:
-                playerPos += dieRoll;
-                if (playerPos < 0) {
-                    playerPos = 0;
-                }
-                System.out.println("Snake: Player moves behind to position " + playerPos);
-                break;
+            System.out.println("Player rolls the die and gets: " + dieRoll);
+
+            switch (option) {
+                case 0:
+                    System.out.println("No Play: Player stays at position " + playerPos);
+                    break;
+                case 1:
+                    playerPos += dieRoll;
+                    if (playerPos > winningPos) {
+                        playerPos -= dieRoll;
+                    }
+                    System.out.println("Ladder: Player moves ahead to position " + playerPos);
+                    break;
+                case 2:
+                    playerPos -= dieRoll;
+                    if (playerPos < 0) {
+                        playerPos = 0;
+                    }
+                    System.out.println("Snake: Player moves behind to position " + playerPos);
+                    break;
+            }
         }
+
+        System.out.println("Congratulations! Player reaches the winning position: " + playerPos);
     }
 }
